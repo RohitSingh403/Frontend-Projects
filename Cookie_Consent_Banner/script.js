634714a7357f1d9e8055a754d4465d90231d401a
Rohit Singh <rohit.singh43@outlook.com>
2026-06-17 19:33:33 +0530

    add basic js logic

---
+++ Cookie_Consent_Banner/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // 1. Target the operational DOM interactive variables
+    const cookieBanner = document.getElementById('cookieBanner');
+    const acceptCookiesBtn = document.getElementById('acceptCookiesBtn');
+    const closeBannerBtn = document.getElementById('closeBannerBtn');
+});
